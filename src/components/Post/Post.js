f5454a6e725756bb22a7e378c2eb5ef4ec867a73
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-22 20:47:00 +0300

    Post.js- material ui eklendi

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,15 +1,103 @@
-import React from "react";
-import "./Post.scss";
+import React, {useState} from "react";
+//import "./Post.scss";
 //import React, {useState,useEffect} from "react";
 //import ReactDOM from "react-dom";
+import { makeStyles } from '@material-ui/core/styles';
+
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import clsx from 'clsx';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: 800
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+      background: red[500],
+    },
+  }));
 
 function Post(props){
     const{title, text} = props;
-
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
     return(
         <div className="postContainer">
-            {title}
-            {text}
+<Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+        subheader="September 14, 2016"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary" componenet="p">
+          {text}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+         className={clsx(classes.expand,{[classes.expandOpen]: expanded,
+        })}
+         // expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+       
+        </CardContent>
+      </Collapse>
+    </Card>
+
         </div>
     )
 }
