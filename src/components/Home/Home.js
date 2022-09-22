a8d38f883842def61861c9404a2a58ce8b0d6400
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-22 20:47:10 +0300

    Home.js - material ui eklendi

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,25 @@
 import React, {useState,useEffect} from "react";
-import Post from "../Post/Post"
+import Post from "../Post/Post";
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent : "center",
+        alignItems : "center",
+        backgroundColor: '#cfe8fc', 
+        height: '100vh'
+    }
+}));
 
 function Home(){
     const [error, setError] = useState(null);
     const[isLoaded, setIsLoaded] = useState(false);
     const[postList, setPostList] = useState([]);
-
+    const classes = useStyles();
+    
     useEffect(() => {
         fetch("/posts")
         .then(res => res.json())
@@ -27,13 +41,11 @@ function Home(){
         return <div> Loading... </div>
     }else{
         return(
-            <div className="container">
-            Home !!!
-
+            <Container fixed className={classes.container}>
                 {postList.map(post =>  (
                         <Post title={post.title} text={post.text} ></Post>
                 ))}
-         </div>
+             </Container>
         );
     }
 
