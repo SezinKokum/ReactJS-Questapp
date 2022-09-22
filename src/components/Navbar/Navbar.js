84482832c459af67d6be999efd12ae51f6ca900f
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-22 20:46:32 +0300

    Navbar.js - material ui eklendi

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,49 @@
+import { AppBar, Button, IconButton, Typography, Toolbar } from "@material-ui/core";
 import React from "react";
 import {Link} from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu"
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        fkexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        textAlign:"left"
+    },
+    link:{
+        textDecoration : "none",
+        boxShadow : "none",
+        color : "white"
+    }
+}));
 function Navbar(){
     let userId = 5;
+    const classes = useStyles();
+
     return(
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to={'/users/'  + userId}>User</Link></li>
-            </ul>
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+          <Link className={classes.link} to="/">Home</Link>
+          </Typography>
+          <Typography variant="h6">
+          <Link className={classes.link} to={'/users/'  + userId}>User</Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
         </div>
     )
 }
